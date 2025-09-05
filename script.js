6cbef1d416759c525bd5fa7f3c4096f60723ee3b
Fehvarella2009 <fernandocarlosvarella@gmail.com>
2025-09-04 22:11:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // Lista de jogadores do Real Value (coloque os pontos reais depois)
 const players = [
-  { name: "FrostSnow", points: 18 },
-  { name: "Ygor BB", points: 12 },
-  { name: "Asky", points: 8 },
-  { name: "Sprout", points: 21,5 },
-  { name: "Icefox", points: 11 },
+  { name: "Fernando", points: 10 },
+  { name: "Geovanna", points: 8 },
+  { name: "Eduardo", points: 6 },
+  { name: "Lívia", points: 6 },
+  { name: "Manuela", points: 9 },
+  { name: "Davi", points: 3 },
+  { name: "Aparecida", points: 2 },
+  { name: "Pietra", points: 1 },
+  { name: "Natally", points: 4 },
+  { name: "Xiao-Jun", points: 5 },
+  { name: "Camila", points: 22 },
+  { name: "Maju", points:18 }
+];
   // Adicione mais nomes aqui se necessário
 ];
 
